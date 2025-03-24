706d8a6425219ac6b2f3530be93796811c54694e
Elvira Ericsson <elvira.rigi@hotmail.com>
2025-03-24 14:39:40 +0100

    js file

---
+++ js/index.js
@@ -0,0 +1,12 @@
+// const firebaseConfig = {
+//     apiKey: "DIN_API_KEY",
+//     authDomain: "DITT_PROJEKT.firebaseapp.com",
+//     projectId: "DITT_PROJEKT",
+//     storageBucket: "DITT_PROJEKT.appspot.com",
+//     messagingSenderId: "DITT_ID",
+//     appId: "DIN_APP_ID"
+// };
+// firebase.initializeApp(firebaseConfig);
+// const storage = firebase.storage();
+// const database = firebase.database();
+
